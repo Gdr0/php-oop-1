3bfd47edd391817ab9a8a52e198b1a1fbea1306e
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-02-03 21:13:47 +0100

    correzione "language" aggiunto funzione per "lead"

--- index.php
+++ index.php
@@ -6,18 +6,24 @@ class Movie {
     public $name;
     public $director;
     public $genres;
+    public $year;
+    public $lead;
     public static $language = "English";
 
-    public function __construct($name,$director,$genres,$year){
+    public function __construct($name,$director,$genres,$year,$lead){
         $this-> name = $name;
         $this-> director = $director;
         $this-> genres = $genres;
         $this -> year = $year;
+        $this -> lead = $lead;
+        $this -> language = self::$language;
     }
     public function addGenre($newGenre){
         $this->genres[] = $newGenre;
     }
- 
+ public function addLead($lead){
+   $this -> lead = $lead;
+ }
 
   public function addYear($year){
     $this -> year = $year;
@@ -26,21 +32,16 @@ class Movie {
 
 
 
-    public static function getLanguage(){
-        return self :: $language;
-    }
-
-
-
 }
-$Movie1 = new Movie ("Seven", "David Fincher", ["thriller"], null) ;
-$Movie2 = new Movie ("Raging Bull","Martin Scorsese", ["biopic","drama"] , null);
+$Movie1 = new Movie ("Seven", "David Fincher", ["thriller"], null, null);
+$Movie2 = new Movie ("Raging Bull","Martin Scorsese", ["biopic","drama"] , null, null,);
 
 
 
 $Movie1->addGenre("drama");
 $Movie1->addGenre("noir");
 $Movie1-> addYear("1995");
+$Movie1-> addLead('Brad Pitt');
 var_dump($Movie1);
 echo "<br>";
 var_dump($Movie1 -> name);
@@ -48,8 +49,19 @@ echo "<br>";
 var_dump($Movie1 -> director);
 echo "<br>";
 var_dump($Movie1 -> genres);
+echo "<br>";
+var_dump($Movie1-> year);
+echo "<br>";
+var_dump($Movie1-> lead);
+echo "<br>";
+echo $Movie1->language;
+echo "<br>";
+
+
 echo "<br>-------------------<br>";
 $Movie2-> addYear("1980");
+$Movie2 -> addLead("Robert De Niro");
+"<br>";
 var_dump($Movie2);
 echo "<br>";
 var_dump($Movie2-> name);
@@ -58,3 +70,8 @@ var_dump($Movie2 -> director);
 echo "<br>";
 var_dump($Movie2 -> genres);
 echo "<br>";
+var_dump($Movie2-> year);
+echo "<br>";
+var_dump($Movie2-> lead);
+echo "<br>";
+echo $Movie2->language;
